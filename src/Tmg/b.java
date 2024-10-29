a10e453a5f3a897a0ba834084e99e08bcbad71ac
Jesper Ekstedt <jemaek@gmail.com>
2024-10-29 13:26:06 +0100

    jespersBranch, klass b, JFrame

--- src/Tmg/b.java
+++ src/Tmg/b.java
@@ -1,9 +1,32 @@
 package Tmg;
 
-public class b {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+public class b extends JFrame {
+    JPanel panel = new JPanel();
+    JButton button1 = new JButton("Button1");
+    JButton button = new JButton("Button2");
+    b(){
+        button.addActionListener(buttonClicked -> System.out.println("klickad"));
+        button1.addActionListener(new ActionListener() {                         // Anonym klass variant.
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("knapp2 klickad");
+            }
+        });
+    }
+
     public static void main(String[] args) {
+        b b = new b();
+
         System.out.println("detta är b  ");
         System.out.println("stämmer bra de");
         System.out.println("skönt");
+
+
+
     }
 }
